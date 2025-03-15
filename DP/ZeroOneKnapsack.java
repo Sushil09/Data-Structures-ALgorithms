411f624fbeae01dd7178fc6e8c5569d505d6461d
Sushil Jaiswal <sjsushil09@gmail.com>
2025-03-15 22:38:35 +0530

    Update ZeroOneKnapsack.java

--- DP/ZeroOneKnapsack.java
+++ DP/ZeroOneKnapsack.java
@@ -1,22 +1,46 @@
 package Questions.DP_15;
 
-public class ZeroOneKnapsack {
-    public static void main(String[] args) {
-        int[]profits={60, 100, 120};//profits
-        int[]weights={10,20,30};//weights
-        int capacity=50;//capacity
-        System.out.println(maxProfit(profits,weights,capacity));
+class Solution {
+    static int knapsack(int W, int val[], int wt[]) {
+        int N = val.length;
+        int[][]dp = new int[N+1][W+1];
+        
+        for(int i=0 ;i<=N;i++)
+            for(int j=0;j<=W;j++)
+                dp[i][j]=-1;
+                
+        // return knapsackUtil(wt,val,W,N,dp);
+        return knapsackUtilTopDown(wt,val,W,N,dp);
     }
-    static int maxProfit(int[]profits,int[]weights,int capacity){
-        int[][]dp=new int[profits.length+1][capacity+1];
-        for(int i=1;i<=profits.length;i++){
-            for(int j=1;j<=capacity;j++){
-                if(j>=weights[i-1])
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-weights[i-1]]+profits[i-1]);
-                else
-                    dp[i][j]=dp[i-1][j];
+    
+    private static int knapsackUtil(int []wt, int[]val, int W, int N, int[][]dp) {
+        if(W<=0 || N<=0)
+            return dp[N][W] = 0;
+        
+        if (dp[N][W] != -1)
+            return dp[N][W];
+        
+        if (wt[N-1]<=W) 
+            return dp[N][W] = Math.max(val[N-1] + knapsackUtil(wt,val,W-wt[N-1],N-1,dp),
+                                        knapsackUtil(wt,val,W,N-1,dp));
+        
+        else
+            return dp[N][W] = knapsackUtil(wt,val,W,N-1,dp);
+    }
+    
+    private static int knapsackUtilTopDown(int []wt, int[]val, int W, int N, int[][]dp) {
+        for(int i=0;i<=N;i++) {
+            for(int j=0;j<=W;j++) {
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+                else{
+                    if(wt[i-1] <=j){
+                        dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                    }else
+                        dp[i][j]=dp[i-1][j];
+                }    
             }
         }
-        return dp[profits.length][capacity];
+        return dp[N][W];
     }
 }
